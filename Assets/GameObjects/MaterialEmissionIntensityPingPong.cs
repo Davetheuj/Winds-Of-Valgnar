66df9a622bd5a91e50a0fe0fc960c86006d398b0
David Underwood-Jett <=>
2022-01-05 16:40:20 -0800

    HDRP _EmissiveIntensity adjustment fix

--- Assets/GameObjects/MaterialEmissionIntensityPingPong.cs
+++ Assets/GameObjects/MaterialEmissionIntensityPingPong.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class MaterialEmissionIntensityPingPong : MonoBehaviour
 {
     private float emissionIntensity;
-    public Material emissiveMaterial;
+    private Material emissiveMaterial;
     public float maxIntensity;
     public float minIntensity;
-    private bool isRising;
-    public float lerpSpeedMultiplier = 1; //You can adjust this in the editor just don't make it 0 or there wont be any changes.
+    public bool isRising;
+    public float lerpSpeedMultiplier; //You can adjust this in the editor just don't make it 0 or there wont be any changes.
+    public float transitionSpeed;
 
+    private void Start()
+    {
+        emissiveMaterial = GetComponent<Renderer>().material;
+            }
     // Update is called once per frame
     void Update()
     {
         if (isRising) 
         {
-            emissiveMaterial.SetFloat("Emissive Intensity", Mathf.Lerp(emissionIntensity, maxIntensity, Time.deltaTime * lerpSpeedMultiplier));
+            emissionIntensity += transitionSpeed *Time.deltaTime;
+           
         }
         else
         {
-            emissiveMaterial.SetFloat("Emissive Intensity", Mathf.Lerp(emissionIntensity, minIntensity, Time.deltaTime * lerpSpeedMultiplier));
+            emissionIntensity -= transitionSpeed * Time.deltaTime;
         }
-        if(emissionIntensity<-minIntensity && !isRising)
+        emissiveMaterial.SetFloat("_EmissiveIntensity", emissionIntensity);
+        //Debug.Log($"Emissive Intensity: {emissiveMaterial.GetFloat("_EmissiveIntensity")}");
+        if (emissionIntensity<=minIntensity && !isRising)
         {
             isRising = true;
         }
@@ -30,6 +38,25 @@ public class MaterialEmissionIntensityPingPong : MonoBehaviour
         {
             isRising = false;
         }
-        
+
+        UpdateEmissiveColorFromIntensityAndEmissiveColorLDR(emissiveMaterial);
+
+
+    }
+
+    public static void UpdateEmissiveColorFromIntensityAndEmissiveColorLDR(Material material)
+    {
+        const string kEmissiveColorLDR = "_EmissiveColorLDR";
+        const string kEmissiveColor = "_EmissiveColor";
+        const string kEmissiveIntensity = "_EmissiveIntensity";
+
+        if (material.HasProperty(kEmissiveColorLDR) && material.HasProperty(kEmissiveIntensity) && material.HasProperty(kEmissiveColor))
+        {
+            // Important: The color picker for kEmissiveColorLDR is LDR and in sRGB color space but Unity don't perform any color space conversion in the color
+            // picker BUT only when sending the color data to the shader... So as we are doing our own calculation here in C#, we must do the conversion ourselves.
+            Color emissiveColorLDR = material.GetColor(kEmissiveColorLDR);
+            Color emissiveColorLDRLinear = new Color(Mathf.GammaToLinearSpace(emissiveColorLDR.r), Mathf.GammaToLinearSpace(emissiveColorLDR.g), Mathf.GammaToLinearSpace(emissiveColorLDR.b));
+            material.SetColor(kEmissiveColor, emissiveColorLDRLinear * material.GetFloat(kEmissiveIntensity));
+        }
     }
 }

1650636377ab7079d911f42c6025ba1704758aa4
David Underwood-Jett <=>
2023-03-08 01:03:33 -0800

    added lightning ability and related scripts

---
+++ Assets/GameObjects/Abilities/Lightning/Lightning.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+
+    public float incrementDuration;
+    public int incrementor;
+    private float time;
+    public Transform startPos;
+    public Transform endPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.parent = GameObject.Find("Main Camera").transform;
+        transform.position  = transform.parent.position + new Vector3(0, -.5f, 1);
+        transform.rotation = transform.parent.rotation;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+
+
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<NPC>() != null)
+        {
+            NPC npc = collision.gameObject.GetComponent<NPC>();
+
+            
+            npc.DealDamageToNpc(10);
+
+            //Instantiate(hitParticle, col.ClosestPoint(transform.position), Quaternion.LookRotation(col.transform.position - col.ClosestPoint(transform.position)));
+
+            //console.AddConsoleMessage1($"You deal {damage} damage to {npc.npcName}!");
+
+            GameObject.Find("Player").GetComponent<StatsController>().GrantXPAndCheckIfLevelGained(100 , "Destruction");
+            //isAttacking = false;
+        }
+
+    }
+
+}
